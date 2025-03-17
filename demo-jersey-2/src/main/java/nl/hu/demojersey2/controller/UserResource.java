70a8195efb94a516252af8c83d4eb5fb224e8f2e
Gilbert563307 <ssempijja759@gmail.com>
2025-03-17 10:18:06 +0100

    working api added

--- demo-jersey-2/src/main/java/nl/hu/demojersey2/controller/UserResource.java
+++ demo-jersey-2/src/main/java/nl/hu/demojersey2/controller/UserResource.java
@@ -1,22 +1,57 @@
 package nl.hu.demojersey2.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import nl.hu.demojersey2.model.User;
+
+import java.util.ArrayList;
 
 @Path("/users")
 public class UserResource {
+    private ArrayList<User> usersList = new ArrayList<>() {{
+         add(new User(1, "Lucas"));
+         add(new User(2, "Mirko"));
+         add(new User(3, "Pim"));
+         add(new User(4, "Jos"));
+         add(new User(5, "Pim"));
+         add(new User(6, "Mirko"));
+    }};
+
+
+
     @GET
     @Produces("application/json")
     public  String users(){
-        return "{ \"name\" : \"John Doe\" }";
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonArray;
+        try {
+            jsonArray = objectMapper.writeValueAsString(this.usersList);
+            return jsonArray;
+        } catch (JsonProcessingException e) {
+            return "{'users': [], 'message': \"" + e.getMessage() + "\"}";
+        }
     }
 
 
     @GET
     @Path("/{id}")
     public String getUserById(@PathParam("id") Long id) {
-        return  "{ \"name\" : \"John Doe\" }";
+        for (User user : this.usersList) {
+            if (user.getId() == id) {
+                try {
+                    ObjectMapper mapper = new ObjectMapper();
+                    return mapper.writeValueAsString(user);
+                } catch (Exception e) {
+                    return "{'user': {}, 'message': \"" + e.getMessage() + "\"}";
+                }
+            }
+        }
+        return "{'user': {}, 'message': ''}";
     }
 }
--- demo-jersey-2/src/main/java/nl/hu/demojersey2/model/User.java
+++ demo-jersey-2/src/main/java/nl/hu/demojersey2/model/User.java
@@ -1,4 +1,20 @@
 package nl.hu.demojersey2.model;
 
 public class User {
+    private int id;
+    private String userName;
+
+    public User(int id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
 }
